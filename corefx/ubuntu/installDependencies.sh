9bc41be99836353a1f05808de96c3c728313cc44
Saurabh Singh <sausing@microsoft.com>
2016-07-15 14:50:03 -0700

    Adding link for the documentation

--- corefx/ubuntu/installDependencies.sh
+++ corefx/ubuntu/installDependencies.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+#The dependecies are listed at https://github.com/dotnet/corefx/blob/master/Documentation/building/unix-instructions.md 
 sudo apt-get install git clang-3.5 cmake make libc6-dev libssl-dev libkrb5-dev libcurl4-openssl-dev zlib1g-dev libunwind8 libicu52 curl
 
 
