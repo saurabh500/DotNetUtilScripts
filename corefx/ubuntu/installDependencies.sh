6d655bbe197620371f462ba3eb9dbe4bd1de0d37
Saurabh Singh <sausing@microsoft.com>
2016-07-15 14:48:12 -0700

    Adding the script env

---
+++ corefx/ubuntu/installDependencies.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+sudo apt-get install git clang-3.5 cmake make libc6-dev libssl-dev libkrb5-dev libcurl4-openssl-dev zlib1g-dev libunwind8 libicu52 curl
+
+
--- corefx/unix/build/cloneAndBuild.sh
+++ corefx/unix/build/cloneAndBuild.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+
 git clone https://github.com/dotnet/corefx && cd corefx && ./build.sh skiptests
 
